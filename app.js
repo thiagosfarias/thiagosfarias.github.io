221c122b6d2bb601296b37e991bdcacf2ba8fec8
thiagosfarias <thiago.farias@acad.ifma.edu.br>
2021-01-31 01:56:58 -0300

    hover na imagem com info, transicoes e animacoes, adicao do card projetos

--- app.js
+++ app.js
@@ -5,3 +5,28 @@ function agrotools(){
 function ifma(){
     window.open('https://portal.ifma.edu.br/');
 }
+
+
+const user = document.querySelector('.user');
+const info = document.querySelector('.info');
+const img = document.querySelector('.profile-pic');
+
+user.addEventListener("mouseenter", e => {
+    info.style.display = 'block'
+    img.style.width = "8rem"
+    img.style.height = "8rem"
+    img.style.opacity = "0"
+})
+
+user.addEventListener("mouseleave", e => {
+    console.log('leave');
+    info.style.display = 'none'
+    img.style.width = "10rem"
+    img.style.height = "10rem"
+    img.style.opacity = "1"
+
+})
+
+
+
+
